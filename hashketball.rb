ea904bdcd2e0eabee8b68603bb627b44a2190b87
Shadman Ahmed <shadman@outlook.com>
2020-05-23 22:21:53 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -128,8 +128,6 @@ def game_hash
 end
 
 def num_points_scored(player_name)
+  game_hash[:home][:players].merge(game_hash[:away][:players])
   binding.pry
-  players = game_hash[:home][:players].merge(game_hash[:away][:players])
 end
-
-
